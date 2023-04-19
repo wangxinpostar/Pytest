3828c04575e7bdeff904f040c95dca0625eab317
wangx <1124434059@qq.com>
2023-04-19 08:53:13 +0800

    ok

--- 36.py
+++
@@ -1,9 +0,0 @@
-import turtle as t
-t.pensize(1)
-t.pencolor("blue")
-t.hideturtle()
-for i in range(8):
-    for i in range(5):
-        t.fd(50)
-        t.left(72)
-    t.left(45)
--- test.py
+++
@@ -1,7 +0,0 @@
-c = 0
-for i in range(1, 1817):
-    s = str(i**3)
-    L = list(map(int, s))
-    if i == sum(L):
-        c = c+1
-print(c)
